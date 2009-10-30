acda54a13c7eb51b10274049eec1c86bd3f5be44
Ben Lickly <blickly@eecs>
2009-10-30 10:28:29 -0700

    Tidied up code slightly

--- src/javato/activetesting/HybridRaceTracker.java
+++ src/javato/activetesting/HybridRaceTracker.java
@@ -13,9 +13,9 @@ class CommutativePair implements java.io.Serializable {
 
   CommutativePair(Integer x, Integer y) {
     if (x < y) {
-      this.x = x; this.y = y;
+      this.x = new Integer(x); this.y = new Integer(y);
     } else {
-      this.y = x; this.x = y;
+      this.y = new Integer(x); this.x = new Integer(y);
     }
   }
 
@@ -65,13 +65,6 @@ class Event {
     if (result) {
       System.out.println("Potential race between " + this + " and " + rhs);
     }
-    /*
-    System.out.println("Checking race between " + this + " and " + rhs);
-    System.out.println("(result = " + result + ")");
-    System.out.println("Clock incomparable: " +
-      (!this.vClock.lessThanEqual(rhs.vClock)
-      && !rhs.vClock.lessThanEqual(this.vClock)));
-    */
     return result;
   }
 
@@ -87,44 +80,15 @@ class Event {
   }
 }
 
-class DatabaseQuery {
-  Long memory;
-  Integer thread;
-  //Boolean needWrite;
-
-  DatabaseQuery(Long mem, Integer t, Boolean wr) {
-    this.memory = mem;
-    this.thread = t;
-    //this.needWrite = wr;
-  }
-
-  public int hashCode() {
-    return this.memory.hashCode() + this.thread.hashCode()
-      //+ this.needWrite.hashCode()
-      ;
-  }
-
-  public boolean equals(Object o) {
-    DatabaseQuery rhs = (DatabaseQuery) o;
-    return this.memory.equals(rhs.memory) && this.thread.equals(rhs.thread)
-      //&& this.needWrite.equals(rhs.needWrite)
-      ;
-  }
-}
-
 class HybridRaceTracker {
-  private Map<Long, LinkedList<Event>> eventsAtMem = new HashMap<Long, LinkedList<Event>>();
   private Map<Long, Map<Integer, LinkedList<Event>>> eventDB = new HashMap<Long, Map<Integer, LinkedList<Event>>>();
   private LinkedHashSet<CommutativePair> races = new LinkedHashSet<CommutativePair>();
 
   public void checkRace(Integer iid, Integer thread, Long memory, boolean isWrite, VectorClock vClock, LockSet ls) {
     Event thisEvent = new Event(iid, thread, memory, isWrite, vClock, ls);
-    //*
     Map<Integer, LinkedList<Event>> eventsForThread = eventDB.get(memory);
     if (eventsForThread == null) return;
-    for (Integer otherThread : eventsForThread.keySet()) {
-      if (otherThread.equals(thread)) continue;
-      LinkedList<Event> eventList = eventsForThread.get(otherThread);
+    for (LinkedList<Event> eventList : eventsForThread.values()) {
       for (Event e : eventList) {
         if (e.vClock.lessThanEqual(vClock)) break;
         else if (thisEvent.isRace(e)) {
@@ -134,27 +98,10 @@ class HybridRaceTracker {
         }
       }
     }
-    // */
-    /*
-    LinkedList<Event> eventStack = eventsAtMem.get(memory);
-    if (eventStack == null) {
-      eventStack = new LinkedList<Event>();
-      eventsAtMem.put(memory, eventStack);
-    }
-    for (Event e : eventStack) {
-      if ( thisEvent.isRace(e) ) {
-        CommutativePair thisRace = new CommutativePair(thisEvent.iid, e.iid);
-        races.add(thisRace);
-        System.out.println("Potential race between " + thisRace);
-      }
-    }
-    // */
   }
 
   public void addEvent(Integer iid, Integer thread, Long memory, boolean isWrite, VectorClock vClock, LockSet ls) {
     Event e = new Event(iid, thread, memory, isWrite, vClock, ls);
-    //*
-    //System.out.println("Adding event to db: " + e);
     Map<Integer, LinkedList<Event>> eventsForThread = eventDB.get(memory);
     if (eventsForThread == null) {
       eventsForThread = new HashMap<Integer, LinkedList<Event>>();
@@ -166,15 +113,6 @@ class HybridRaceTracker {
       eventsForThread.put(thread, eventList);
     }
     eventList.addFirst(e);
-    // */
-    /*
-    LinkedList<Event> eventStack = eventsAtMem.get(memory);
-    if (eventStack == null) {
-      eventStack = new LinkedList<Event>();
-      eventsAtMem.put(memory, eventStack);
-    }
-    eventStack.addFirst(e);
-    // */
   }
 
 //    The following method call creates a file "error.list" containing the list of numbers "1,2,3,...,nRaces"
